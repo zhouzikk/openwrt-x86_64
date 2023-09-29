1b9671b4aa1233143b43e6c9e6f9a7b1dd6a71b7
zhouzi <1626797755@qq.com>
2023-09-29 23:27:26 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
